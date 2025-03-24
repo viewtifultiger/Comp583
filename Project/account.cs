884eaa699f55aeacce921d0f649b8ef94dbd4fcd
Michael-S-Powell <michael.powell.108@my.csun.edu>
2025-03-24 15:53:48 -0700

    added to account
    added availableTimes varaible

--- Project/account.cs
+++ Project/account.cs
@@ -4,13 +4,15 @@ public class account{
     int age;
     string account_type;
     string hospital;
+    string [] availableTimes;
     
-    public account(int num, string name, int age, string account_type, string hospital = null){
+    public account(int num, string name, int age, string account_type, string hospital = null, string available= null){
         account_number = num;
         this.name = name;
         this.age=age;
         this.account_type = account_type;
         this.hospital = hospital;
+        this.availableTimes=available;
     }
     
     public int get_account_number(){
@@ -30,6 +32,11 @@ public class account{
             return hospital;
         }
     }
+    public string get_availableTimes(){
+        if(availableTimes!=null){
+            return availableTimes;
+        }
+    }
     public void set_name(string n){
         this.name=n;
     }
@@ -39,6 +46,11 @@ public class account{
     public void set_hospital(string h){
         if(account_type.equals("doctor")){
             this.hospital=h;
+        }
+    }
+    public void set_availableTimes(string a){
+        if(account_type.equals("doctor")){
+            this.availableTimes=a;
         }
     }
 }
