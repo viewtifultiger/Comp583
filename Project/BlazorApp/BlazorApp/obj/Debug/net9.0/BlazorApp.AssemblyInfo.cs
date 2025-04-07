a264e992b6aa6ea8eccfa7c5b6e2d70fd3fafe8b
Michael-S-Powell <michael.powell.108@my.csun.edu>
2025-04-07 16:14:06 -0700

    tested blazor app

--- Project/BlazorApp/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ Project/BlazorApp/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf08568d4a5473b823d0ecbb34bbe9e13ce57ad7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e796b103375672e84e87e49fdb28ddd71358fa13")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
