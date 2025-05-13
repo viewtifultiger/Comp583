60ee65795fc6affe053b10d1765899395d8f1267
Michael-S-Powell <michael.powell.108@my.csun.edu>
2025-05-12 19:00:47 -0700

    modify appointment

--- Project/BlazorApp/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ Project/BlazorApp/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c84817cf8a32dde3b79bc86edb379667074e63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56da8d7a88983ae27be509998ae0a39b128595f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
